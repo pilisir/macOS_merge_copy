e25d08e23ad9122645b86e18be411046f084fef5
jasonjian <jasonjian@flowring.com>
2019-06-05 23:45:08 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# macOS merge copy service
+
+Leave away drag and drop with [option] key and say Goodbye to the "Replace".
+Do Real Merge with simple services on macOS Finder.
+
+[中文版](https://github.com/pilisir/macOS_merge_copy/wiki/%E4%B8%AD%E6%96%87%E7%89%88)
+
+## Feature
+* Copy selected files or folders to a target folder and append as children if not existing or overwrite otherwise. 
+## Install
+There are two way to create the services.
+##### Way 1: Copy and Paste
+1. Directly download this project and uncompress it.
+2. Move the service file to the local service folder. You can easily move directory __*Library*__ under __*./dist/*__ of this project to your local __*$USER*__ directory.  (Please set your Finder eanble to see hidden directories via shortcut key **[Shift] + [Command] + [.]**  first.)
+3. Done !
+##### Way 2: Create services yourself.
+1. Open **Automator** app.
+2. Press **[Command] + [N]** to new a document which type is *Service*.
+3. Select __*Action > Library > Files & Folders > Get Selected Finder Items*__ and drag it to the right side as Step One.
+4. Select __*Action > Library > Utillities > Run Shell Script*__ and drag it to the right side as Step Two.
+5. Drop down **Pass input** of Step Two and select the **as arguments**.
+6. Copy content text of file from this project, __*./src/merge_copy.sh*__, paste it as the Step Two shell content text.
+7. Look up the top, drop down **Service receives selected** and select **files or folders**.
+8. Press **[Command] + [S]** to save this service with specified name "**Merge Copy**" (or any custom name yourself). 
+9. Done !
+
+## Usage
+1. Select files or directories in Finder, open conetxt menu and select Service > **ZIP** (or **ZIP with path**).
+2. That's all !
+![context menu](https://github.com/pilisir/macOS_merge_copy/blob/master/doc/image/contextmenu.png)
+
+## Recommend Setting Hot Key
+* You can assign shortcut key for the service via Keyboard in System Preferences to make more easily.
+* Recommend **[Control] + [Command] + [m]**, meaning as **Merge**.
+* [Tutorial](https://apple.stackexchange.com/questions/43998/how-do-i-assign-a-keyboard-shortcut-to-a-service-in-os-x)
+
+## FYI
+* I never test it lower than Mojave.
+* You may find out the script is not elegant itself, because this is just my second project of Automator with shell script. I look forward to any suggestion or fork.
+
+## License
+MIT License
+
+## Reference
+* (https://grahamrpugh.com/2017/01/07/application-to-run-shell-commands-with-admin-rights.html)
+* (https://stackoverflow.com/questions/39932522/applescript-how-to-get-full-path-of-the-current-finder-window-open)
+
+## Donate
+[Donate me via PayPal](https://www.paypal.me/pilisir/0.99usd)\
+[Donate me via OPay(歐付寶)](https://p.opay.tw/unUun)
---
+++ src/merge_copy.sh
@@ -0,0 +1,94 @@
+# Version 1.0
+# Copyright (c) 2019 pilisir.tw@gmail.com
+# Under MIT Licesne, please go to "https://en.wikipedia.org/wiki/MIT_License" to check license terms.
+
+set +H
+itemsPathArray=()
+lastItemPath=""
+eachItemPathEscape=""
+
+escapePath() {
+	resultValue=$(echo "$@" | sed 's@[\]@\\\\@g;s/"/\\"/g;s/\ /\\ /g;s/'"'"'/\'"\'"'/g;s/`/\\`/g;s/:/\\:/g;s/?/\\?/g;s/!/\\!/g;s/</\\</g;s/>/\\>/g;s/|/\\|/g;s/*/\\*/g;s/(/\\(/g;s/)/\\)/g;s/\[/\\[/g;s/\]/\\]/g;s/{/\\{/g;s/}/\\}/g;s/&/\\&/g;s/%/\\%/g;s/\$/\\$/g;s/#/\\#/g;s/~/\\~/g;s/=/\\=/g;s/,/\\,/g;s@[;]@\\;@g;')
+	echo $resultValue
+}
+
+targetFolder=$(osascript <<EOF
+	tell application "Finder"
+		activate
+	    if exists Finder window 1 then
+	        set currentDir to target of Finder window 1 as alias
+	    else
+	        set currentDir to desktop as alias
+	    end if
+	end tell
+	tell application "System Events"
+    	activate
+		POSIX path of (choose folder with prompt "Please choose the folder of target:" ¬
+	    	default location currentDir)
+	end tell 	
+EOF
+)
+
+if [ -z $targetFolder ]; then
+	exit
+fi
+
+if [[ ${targetFolder: -1} == "/" && ${targetFolder} != "/" ]]; then
+	targetFolder=\""${targetFolder%?}"\"
+fi
+
+for eachItemPath in "$@"
+do
+	if [[ $targetFolder == *"$eachItemPath"* || $targetFolder == "${eachItemPath%/*}" ]]; then
+		exit
+	fi
+	eachItemPathEscape=$(escapePath $eachItemPath)
+	itemsPathArray+=($eachItemPathEscape)
+done
+
+eachItemPathEscape=$(escapePath $targetFolder)
+itemsPathArray+=($eachItemPathEscape)
+
+{ 
+	cmd="cp -R ${itemsPathArray[@]}"
+	eval $cmd && echo OK
+} || {
+	# Dialog Title
+	dialogTitle="Require password"
+
+	# obtain the password from a dialog box
+	authPass=$(osascript <<EOT
+	tell application "System Events"
+	    activate
+	    repeat
+	        display dialog "Action requires administrator privileges.\r\nPassword:" ¬
+	            default answer "" ¬
+	            with title "$dialogTitle" ¬
+	            with hidden answer ¬
+	            buttons {"Cancel", "Ok"} default button 2
+	        if button returned of the result is "Cancel" then
+	            return 1
+	            exit repeat
+	        else if the button returned of the result is "Ok" then
+	            set pswd to text returned of the result
+	            set usr to short user name of (system info)
+	            try
+	                do shell script "echo test" user name usr password pswd with administrator privileges
+	                return pswd
+	                exit repeat
+	            end try
+	        end if
+	        end repeat
+	        end tell
+	EOT
+	)
+
+	# Abort if the Quit button was pressed
+	if [ "$authPass" == 1 ]; then
+	    echo "User Canceled. Exit"
+	    exit 0
+	fi
+
+	cmd="echo $authPass | sudo -S cp -a ${itemsPathArray[@]}"
+	eval $cmd
+}
